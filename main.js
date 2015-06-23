3b61fff0fa54ebee339eb72eeacf0448943d03c7
Brutus <dev.brutus@gmail.com>
2015-06-23 22:29:39 +0300

    bmi (no validation)

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
 const app = {
     init: function () {
-        var tabs = $('ul.nav-tabs > li');
+        var tabs = $('.calculator-tab');
+        var forms = $('.calculator-form');
+
         tabs.click(function (event) {
             tabs.removeClass("active");
+            forms.addClass("invisible")
             app.changePage(event);
         });
-        $('#nav-tab-bmi').click(function () {
-            alert("BMI!!!")
+
+        $('#calculator-bmi-run').click(function () {
+            var height = $('#calculator-bmi-height').val();
+            var weight = $('#calculator-bmi-weight').val();
+
+            var h = height / 100.0;
+            var I = weight / ( h * h );
+
+            $('#calculator-bmi-result').val(I)
         })
     },
 
     changePage: function (event) {
-        $(event.delegateTarget).addClass("active");
+        var delegateTarget = event.delegateTarget;
+        $(delegateTarget).addClass("active");
+        var formId = delegateTarget.id.replace('tab', 'form')
+        $('#' + formId).removeClass("invisible")
     }
 };
